b618cad3f599b64c19cb7a3d061ee8626c957e72
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-06-24 16:53:27 +0300

    class task 60/100 (copy and size are wrong)

---
+++ week 9/class_task.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+
+class Matrix:
+    def __init__(self, list_of_lists=None):
+        if list_of_lists is None:
+            list_of_lists = []
+        else:
+            self.list_of_lists = list.copy(list_of_lists)
+
+    def __str__(self):
+        matrix_str = ''
+        for inner_list in self.list_of_lists:
+            for number in inner_list:
+                matrix_str += f"{number}\t"
+            matrix_str = matrix_str[0:-1]
+            matrix_str += '\n'
+
+        matrix_str = matrix_str[0:-1]
+
+        return matrix_str
+
+    def size(self):
+        return len(self.list_of_lists), len(self.list_of_lists[0])
+
+
+exec(stdin.read())
