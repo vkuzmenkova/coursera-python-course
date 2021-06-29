4253d4301bd688884305efda015910e10715aaf5
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-06-29 18:00:26 +0300

    add range instead of for

--- week8/zero_or_not.py
+++ week8/zero_or_not.py
@@ -1,5 +1,1 @@
-n = int(input())
-list_of_ints = []
-for i in range(n):
-    list_of_ints.append(int(input()))
-print(0 in list_of_ints)
+print(0 in map(lambda x: int(input()), range(int(input()))))
