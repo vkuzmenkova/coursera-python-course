8e9bd38599ea792b442cba2bce945f7166c74df1
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-06-30 15:13:02 +0300

    fixes

--- week6/pass_entrance_exam.py
+++ week6/pass_entrance_exam.py
@@ -1,27 +1,34 @@
-file = open('input.txt', 'r', encoding="utf-8")
-text = file.read().split('\n')
-file.close()
-
-k = int(text[0])
-text.remove(text[0])
+in_file = open("input.txt", "r", encoding="utf8")
+k = int(in_file.readline())
 results = []
 
+for line in in_file:
+    newLine = line.split()
+    if int(newLine[-1]) >= 40 and int(newLine[-2]) >= 40 \
+            and int(newLine[-3]) >= 40:
+        results.append(newLine)
 
-def scores_sum(x):
-    return x[0] + x[1] + x[2]
+in_file.close()
+
+results.sort(key=lambda a: int(a[-1]) + int(a[-2]) + int(a[-3]), reverse=True)
+
+final = []
+for i in results:
+    sum = int(i[-1]) + int(i[-2]) + int(i[-3])
+    final.append(sum)
+n = len(final)
 
 
-for line in text:
-    temp = list(map(int, line.split()[-3:]))
-    if temp[0] >= 40 and temp[1] >= 40 and temp[2] >= 40:
-        results.append(temp)
+def contest(n, k, in_list):
+    if n <= k:
+        return 0
+    elif in_list[k] == in_list[0]:
+        return 1
+    for i in range(k, 0, -1):
+        if in_list[i] < in_list[i - 1]:
+            return in_list[i - 1]
 
-results.sort(key=scores_sum, reverse=True)
-final_scores = list(map(scores_sum, results))
-passed_score = final_scores[k - 1]
-if final_scores.count(passed_score) > 1:
-    passed_score = final_scores[final_scores.index(passed_score) - 1]
 
-file = open('output.txt', 'w', encoding='utf-8')
-file.write(str(passed_score))
-file.close()
+out_file = open("output.txt", "w", encoding="utf8")
+out_file.write(str(contest(n, k, final)))
+out_file.close()
